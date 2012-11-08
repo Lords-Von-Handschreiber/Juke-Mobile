e455e4eae5c16fbc76e52603ed0e49ebb866dab9
Christian Bachmann <cbwerbung@inaffect.net>
2012-11-08 22:07:20 +0100

    Model updated

---
+++ Juke-Mobile-Model/MusicInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juke_Mobile_Model
+{
+    public class MusicInfo
+    {
+        public String Artist{ get; set;}
+        public String Album { get; set; }
+        public String Title { get; set; }
+
+        public override string ToString()
+        {
+            return Artist + ", " + Title + ", " + Album;
+        }
+    }
+}
