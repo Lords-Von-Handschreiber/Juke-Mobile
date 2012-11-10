adbd7f2e69d3e7db6879c2b4f7e014d33d71d4a9
Thomas Bandixen <thomas@bandixen.ch>
2012-11-10 18:04:53 +0100

    Client GUI updated

---
+++ Juke-Mobile-Client/js/client.js
@@ -0,0 +1,50 @@
+var lsUsernameKey = 'juke-mobile.juker';
+
+function IsNullOrEmpty(thing) {
+    if (!thing)
+        return true;
+    if (thing == '')
+        return true;
+    return false;
+}
+
+function IsUserNameSet() {
+    return !IsNullOrEmpty(GetUserName());
+}
+
+function GetUserName() {
+    var userName = localStorage.getItem(lsUsernameKey);
+    if (userName)
+        return userName;
+    else
+        return sessionStorage.getItem(lsUsernameKey);
+}
+
+$(document).ready(function () {
+    if (!Modernizr.localstorage) {
+        alert('Leider unterstützt den Browser den Localstorage nicht');
+        return;
+    }
+
+    if ($('#signin-page').length > 0) {
+        if (IsUserNameSet()) {
+            window.location.href = 'list.html';
+        }
+
+        $('.form-signin').submit(function () {
+            var userName = $('#username').val();
+            if (userName != '') {
+                if ($('#remember').attr('checked'))
+                    localStorage.setItem(lsUsernameKey, userName);
+                else
+                    sessionStorage.setItem(lsUsernameKey, userName);
+
+                window.location.href = 'list.html';
+            } else {
+            }
+            return false;
+        });
+    } else if (!IsUserNameSet()) {
+        window.location.href = 'index.html';
+    }
+});
