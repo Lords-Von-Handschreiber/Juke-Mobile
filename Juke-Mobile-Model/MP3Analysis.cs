3c3bda1d98852dae8d214ee1c00ef542f5a9b94a
Christian Bachmann <cbwerbung@inaffect.net>
2012-11-16 22:06:31 +0100

    Mein erster Commit

---
+++ Juke-Mobile-Model/MP3Analysis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juke_Mobile_Model
+{
+    public class MP3Analysis
+    {
+        public int makethat(int t)
+        {
+            return t+1;
+        }
+    }
+}
---
+++ Juke-Mobile-UnitTest/MusicAnalysisTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Juke_Mobile_Model;
+
+namespace Juke_Mobile_UnitTest
+{
+    [TestClass]
+    public class MusicAnalysisTest
+    {
+        public const string scheisse = "Scheisse";
+        [TestMethod]
+        public void TestT()
+        {
+            MP3Analysis mp3a = new MP3Analysis();
+            int that=mp3a.makethat(1);           
+            if (mp3a.makethat(1) != 1)
+            {
+                throw new ArgumentOutOfRangeException("test that", that, scheisse);
+            }
+        }
+    }
+}
